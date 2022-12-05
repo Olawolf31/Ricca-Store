c1de9a8ff3d1559aaecde6e58a713a789739fe87
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2022-12-05 20:10:21 +0100

    setting up boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Route>
+        <Routes />
+
+        <Routes />
+      </Route>
     </div>
   );
 }
