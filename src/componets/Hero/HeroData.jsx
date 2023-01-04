a1ec8723be7a64591775741dc1318453a4567cb7
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-01-05 00:35:24 +0100

    fixed slider images

--- src/componets/Hero/HeroData.jsx
+++ src/componets/Hero/HeroData.jsx
@@ -1,7 +1,7 @@
 export const heroData = [
   {
     id: 1,
-    url: "https://pixabay.com/get/ge8f4e988446ce9d9f6d74587092d32bca8aee90d98e57d5c2c8c3ba8f06e89e4e0d52958354eb01c5cb67eb0bcd0b7ff68735f137afa5a4fea937b060dc0e34e01d1669a823e86ce161aaeb74d1a45ee_1920.jpg",
+    url: "https://i.postimg.cc/TYFq24zy/bruno-gomiero-jp7-J14-W9s-Sg-unsplash.jpg",
     title: "Get the latest and greatest smartphones at our store!",
     description: "Stay connected with the latest technology in 2023",
     className:
@@ -9,7 +9,7 @@ export const heroData = [
   },
   {
     id: 2,
-    url: "https://pixabay.com/get/g6faf3c3c85f36ec1058af69c5b207cd585db0d0739bdb61414d04205408e0bb80e1ef73913d1a27b4d16b361789d6bfea1d1deba55ad37e95d8883f512df67d3bd554b1e9beae506e45704e122ff8f44_1920.jpg",
+    url: "https://i.postimg.cc/W3Gf0vPf/arnel-hasanovic-4o-WSXde-AS2g-unsplash.jpg",
     title: "Upgrade your phone and your life with our wide selection of phones!",
     description: "Find the phone that fits your needs and budget.",
     className:
@@ -17,7 +17,7 @@ export const heroData = [
   },
   {
     id: 3,
-    url: "https://pixabay.com/get/g7ac25f3a1f4303f900a9a347633c8777d928ab967e25f0920c7a0454d09a7b78c8c467f798d0c63773d824fe4d851d59cd257f21d569b8d3112ec3275166fecebfe1c27b3bae7024b5689ac19f65d4f7_1920.jpg",
+    url: "https://i.postimg.cc/J0tZMNKc/bagus-hernawan-A6-Jx-K37-Il-Po-unsplash.jpg",
     title: "Find the perfect phone for you at our mobile phone store!",
     description: "Experience the power of a new smartphone today",
     className:
